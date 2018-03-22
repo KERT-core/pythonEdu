0f7f6d1015b9969ef1c5015e5ae2ae94d70f5f99
unknown <ekstrah2013@kert.or.kr>
2018-03-22 16:42:38 +0900

    Problem 1 test case fixed

--- chapter1/problem1/test.py
+++ chapter1/problem1/test.py
@@ -2,8 +2,22 @@ from solutions import solution
 import os
 
 def test_case_1():
-    answer = b'$2b$12$hVEug1duGIuQotj37kGtUOH7P.qSSCePZyaHJy6IAbuOxbl0mWxFO'
-    assert solution('korea') == answer
-    
+    assert solution(123) == 321
+
+def test_case_2():
+     assert solution(-123) == -321
+
+def test_case_3():
+    assert solution(120) == 21
+
+def test_case_4():
+    assert solution(-1827) == -7281
+
+def test_case_5():
+    assert solution(0) == 0
+
+def test_case_6():
+    assert solution(-0) == 0
+
 if __name__ == "__main__":
     os.system("py.test test.py")
